7e1f040c1d49fcea030271c7e059be4933e6f9fa
AnonymousXDragon <rohitAsAnonymous@proton.me>
2025-08-24 11:00:01 +0530

    commit#8: pipeline coverage test  bug fixed

--- config/tasks.py
+++ config/tasks.py
@@ -1,10 +1,16 @@
+import os
 from celery import Celery
 from django.conf import settings
 from django_redis import get_redis_connection
 from decouple import config
 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 redis_client = get_redis_connection('default')
-app = Celery('tasks', broker=f'redis://{config('REDIS_CLIENT')}/1')
+app = Celery('trades', broker=f'redis://{config('REDIS_CLIENT')}/1')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 @app.task
 def check():
