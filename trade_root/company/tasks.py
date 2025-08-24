0333eedf00e4cd024e7706e78ee4ea9ae5e21e5a
AnonymousXDragon <rohitAsAnonymous@proton.me>
2025-08-24 16:22:23 +0530

    commit#24: bugs in pipeline

--- trade_root/company/tasks.py
+++ trade_root/company/tasks.py
@@ -2,7 +2,10 @@
 from celery import shared_task
 from .models import Company
 from django.db import transaction
+from .logger import log
+
 import random
+
 
 
 @shared_task
@@ -13,9 +16,12 @@ def update_company_price_on_schedule():
         with transaction.atomic():
             for company in companies:
                 random_price = round(random.uniform(1,1000),2)
+                log.info(f"Price is triggerd : {random_price}")
                 company.price = random_price
+                log.info(f"Price is set to price field of company : {random_price}")
             
             Company.objects.bulk_update(companies,['price'])
+            log.info(f"Price updating completed : {random_price}")
         
     except Exception as err:
         print(str(err))
